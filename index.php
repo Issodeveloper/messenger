a25d8ddd7cca1ae9644ddc03de413fefbab20fe3
Issodeveloper <93887705+Issodeveloper@users.noreply.github.com>
2022-04-05 06:51:40 +0200

    inti

---
+++ index.php
@@ -0,0 +1,247 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="./src/css/all.css">
+    <link rel="stylesheet" href="./src/css/messenger.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <title>Firedump messenger</title>
+</head>
+<body>
+    <section class="messages-open-container"></section>
+    <div class="messages-open-else-container">
+        <div class="messenger-container">
+            <div class="messenger-header">Joe Biden</div>
+            <div class="messages-body">
+                <div class="msg-container from-me">
+                    <div class="message">Fuck you mr.president your little fucker idk if you know what youre even doing</div>
+                </div>
+                <div class="msg-container from-other">
+                    <div class="message">😢</div>
+                </div>
+                <div class="msg-container from-other">
+                    <div class="message">Bruh thats crazy, problem is... i didn't ask</div>
+                </div>
+                <div class="msg-container from-me">
+                    <div class="message">Is this a president or are you a fucking dictator mr.president?</div>
+                </div>
+                <div class="msg-container from-other">
+                    <div class="message">Both bitch. Thats the problem with America</div>
+                </div>
+                <div class="msg-container from-other">
+                    <div class="message">Deal with it</div>
+                </div>
+                <div class="msg-container from-me">
+                    <div class="message">You i know where you fucking live 👹</div>
+                </div>
+                <div class="msg-container from-other">
+                    <div class="message">Every fucking body knows where i fucking live, And its 1000 times bigger than youre house</div>
+                </div>
+                <div class="msg-container from-me">
+                    <div class="message">Fuck you mr.president your little fucker idk if you know what youre even doing</div>
+                </div>
+                <div class="msg-container from-other">
+                    <div class="message">😢</div>
+                </div>
+                <div class="msg-container from-other">
+                    <div class="message">Bruh thats crazy, problem is... i didn't ask</div>
+                </div>
+                <div class="msg-container from-me">
+                    <div class="message">Is this a president or are you a fucking dictator mr.president?</div>
+                </div>
+                <div class="msg-container from-other">
+                    <div class="message">Both bitch. Thats the problem with America</div>
+                </div>
+                <div class="msg-container from-other">
+                    <div class="message">Deal with it</div>
+                </div>
+                <div class="msg-container from-me">
+                    <div class="message">You i know where you fucking live 👹</div>
+                </div>
+                <div class="msg-container from-other">
+                    <div class="message">Every fucking body knows where i fucking live, And its 1000 times bigger than youre house</div>
+                </div>
+                <div class="msg-container from-me">
+                    <div class="message">Fuck you mr.president your little fucker idk if you know what youre even doing</div>
+                </div>
+                <div class="msg-container from-other">
+                    <div class="message">😢</div>
+                </div>
+                <div class="msg-container from-other">
+                    <div class="message">Bruh thats crazy, problem is... i didn't ask</div>
+                </div>
+                <div class="msg-container from-me">
+                    <div class="message">Is this a president or are you a fucking dictator mr.president?</div>
+                </div>
+                <div class="msg-container from-other">
+                    <div class="message">Both bitch. Thats the problem with America</div>
+                </div>
+                <div class="msg-container from-other">
+                    <div class="message">Deal with it</div>
+                </div>
+                <div class="msg-container from-me">
+                    <div class="message">You i know where you fucking live 👹</div>
+                </div>
+                <div class="msg-container from-other">
+                    <div class="message">Every fucking body knows where i fucking live, And its 1000 times bigger than youre house</div>
+                </div>
+                <div class="msg-container from-me">
+                    <div class="message">Fuck you mr.president your little fucker idk if you know what youre even doing</div>
+                </div>
+                <div class="msg-container from-other">
+                    <div class="message">😢</div>
+                </div>
+                <div class="msg-container from-other">
+                    <div class="message">Bruh thats crazy, problem is... i didn't ask</div>
+                </div>
+                <div class="msg-container from-me">
+                    <div class="message">Is this a president or are you a fucking dictator mr.president?</div>
+                </div>
+                <div class="msg-container from-other">
+                    <div class="message">Both bitch. Thats the problem with America</div>
+                </div>
+                <div class="msg-container from-other">
+                    <div class="message">Deal with it</div>
+                </div>
+                <div class="msg-container from-me">
+                    <div class="message">You i know where you fucking live 👹</div>
+                </div>
+                <div class="msg-container from-other">
+                    <div class="message">Every fucking body knows where i fucking live, And its 1000 times bigger than youre house</div>
+                </div>
+                <div class="msg-container from-me">
+                    <div class="message">Fuck you mr.president your little fucker idk if you know what youre even doing</div>
+                </div>
+                <div class="msg-container from-other">
+                    <div class="message">😢</div>
+                </div>
+                <div class="msg-container from-other">
+                    <div class="message">Bruh thats crazy, problem is... i didn't ask</div>
+                </div>
+                <div class="msg-container from-me">
+                    <div class="message">Is this a president or are you a fucking dictator mr.president?</div>
+                </div>
+                <div class="msg-container from-other">
+                    <div class="message">Both bitch. Thats the problem with America</div>
+                </div>
+                <div class="msg-container from-other">
+                    <div class="message">Deal with it</div>
+                </div>
+                <div class="msg-container from-me">
+                    <div class="message">You i know where you fucking live 👹</div>
+                </div>
+                <div class="msg-container from-other">
+                    <div class="message">Every fucking body knows where i fucking live, And its 1000 times bigger than youre house</div>
+                </div>
+                <div class="msg-container from-me">
+                    <div class="message">Fuck you mr.president your little fucker idk if you know what youre even doing</div>
+                </div>
+                <div class="msg-container from-other">
+                    <div class="message">😢</div>
+                </div>
+                <div class="msg-container from-other">
+                    <div class="message">Bruh thats crazy, problem is... i didn't ask</div>
+                </div>
+                <div class="msg-container from-me">
+                    <div class="message">Is this a president or are you a fucking dictator mr.president?</div>
+                </div>
+                <div class="msg-container from-other">
+                    <div class="message">Both bitch. Thats the problem with America</div>
+                </div>
+                <div class="msg-container from-other">
+                    <div class="message">Deal with it</div>
+                </div>
+                <div class="msg-container from-me">
+                    <div class="message">You i know where you fucking live 👹</div>
+                </div>
+                <div class="msg-container from-other">
+                    <div class="message">Every fucking body knows where i fucking live, And its 1000 times bigger than youre house</div>
+                </div>
+                <div class="msg-container from-me">
+                    <div class="message">Fuck you mr.president your little fucker idk if you know what youre even doing</div>
+                </div>
+                <div class="msg-container from-other">
+                    <div class="message">😢</div>
+                </div>
+                <div class="msg-container from-other">
+                    <div class="message">Bruh thats crazy, problem is... i didn't ask</div>
+                </div>
+                <div class="msg-container from-me">
+                    <div class="message">Is this a president or are you a fucking dictator mr.president?</div>
+                </div>
+                <div class="msg-container from-other">
+                    <div class="message">Both bitch. Thats the problem with America</div>
+                </div>
+                <div class="msg-container from-other">
+                    <div class="message">Deal with it</div>
+                </div>
+                <div class="msg-container from-me">
+                    <div class="message">You i know where you fucking live 👹</div>
+                </div>
+                <div class="msg-container from-other">
+                    <div class="message">Every fucking body knows where i fucking live, And its 1000 times bigger than youre house</div>
+                </div>
+                <div class="msg-container from-me">
+                    <div class="message">Fuck you mr.president your little fucker idk if you know what youre even doing</div>
+                </div>
+                <div class="msg-container from-other">
+                    <div class="message">😢</div>
+                </div>
+                <div class="msg-container from-other">
+                    <div class="message">Bruh thats crazy, problem is... i didn't ask</div>
+                </div>
+                <div class="msg-container from-me">
+                    <div class="message">Is this a president or are you a fucking dictator mr.president?</div>
+                </div>
+                <div class="msg-container from-other">
+                    <div class="message">Both bitch. Thats the problem with America</div>
+                </div>
+                <div class="msg-container from-other">
+                    <div class="message">Deal with it</div>
+                </div>
+                <div class="msg-container from-me">
+                    <div class="message">You i know where you fucking live 👹</div>
+                </div>
+                <div class="msg-container from-other">
+                    <div class="message">Every fucking body knows where i fucking live, And its 1000 times bigger than youre house</div>
+                </div>
+                <div class="msg-container from-me">
+                    <div class="message">Fuck you mr.president your little fucker idk if you know what youre even doing</div>
+                </div>
+                <div class="msg-container from-other">
+                    <div class="message">😢</div>
+                </div>
+                <div class="msg-container from-other">
+                    <div class="message">Bruh thats crazy, problem is... i didn't ask</div>
+                </div>
+                <div class="msg-container from-me">
+                    <div class="message">Is this a president or are you a fucking dictator mr.president?</div>
+                </div>
+                <div class="msg-container from-other">
+                    <div class="message">Both bitch. Thats the problem with America</div>
+                </div>
+                <div class="msg-container from-other">
+                    <div class="message">Deal with it</div>
+                </div>
+                <div class="msg-container from-me">
+                    <div class="message">You i know where you fucking live 👹</div>
+                </div>
+                <div class="msg-container from-other">
+                    <div class="message">Every fucking body knows where i fucking live, And its 1000 times bigger than youre house</div>
+                </div>
+            </div>
+            <form action="./" action="POST" class="send-form">
+                <div class="send-msg-container">
+                    <input type="text" class="input-msg" placeholder="Message">
+                    <input type="submit" class="send-btn" placeholder="Message" value="Send">
+                </div>
+            </form>
+        </div>
+    </div>
+</body>
+</html>
